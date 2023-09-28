15840ae7151b44408bc9fae6956f53036e6674ca
Alisa Dmitrenko <alisa_dmitrenko@mail.ru>
2023-09-28 21:26:24 +0300

    add popup

--- src/components/UserCardPopUp/UserCardPopUp.jsx
+++ src/components/UserCardPopUp/UserCardPopUp.jsx
@@ -1,5 +1,6 @@
+import UsersCard from '../UsersCard/UsersCard';
 import './UserCardPopUp.scss';
 
 export default function UserCardPopUp() {
-  return <div className="userCardPopUp"></div>;
+  return <div className="userCardPopUp">UserCardPopUp</div>;
 }
